9e85768b04383223347648d0998003104cedec1a
endorphin82 <endorphin82@gmail.com>
2020-07-24 11:39:17 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -11,20 +11,14 @@ const {values, handleChange} = useMyHook()
   //   const { name, value } = e.target
   //   setValues({ ...values, [name]: value })
   // }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  }
-
+  
   return (
     <div className="App">
       <header className="App-header">
         <p>
         </p>
-        <form onSubmit={handleSubmit}>
           <input value={values['name']} name="name" onChange={handleChange} type="text" /> 
           <input value={values['last_name']} name="last_name" onChange={handleChange} type="text" /> 
-        </form>
       </header>
     </div>
   );
